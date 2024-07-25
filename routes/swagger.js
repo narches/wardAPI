80aeaf7e76e8c5142ad4e2a5375ba08ce6a3b468
JoE <156410520+narches@users.noreply.github.com>
2024-07-25 16:55:12 +0100

    Adjustments

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
-//const swaggerDocument = require('../swagger.json');
+const swaggerDocument = require('../swagger.json');
 
 
 
 
 router.use('/api-docs', swaggerUi.serve);
 
-//router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 
 
--- swagger.js
+++ swagger.js
@@ -1,5 +1,5 @@
 const swaggerAutogen = require('swagger-autogen')();
-//const swaggerDocument = require('./swagger.json');
+const swaggerDocument = require('./swagger.json');
 
 
 const doc = {
